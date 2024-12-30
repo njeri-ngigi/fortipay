1f1ad942947b2687e7591fe0f72582bc18f2ffd2
Shalon N. Ngigi <njery.ngigi@gmail.com>
2024-12-30 11:44:35 +0300

    add folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
+import { LogsModule } from './logs/logs.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { UsersModule } from './users/users.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    UsersModule,
+    ConfigModule,
+    CommonModule,
+    WalletModule,
+    TransactionsModule,
+    LogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
